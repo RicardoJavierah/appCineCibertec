90e8fb21b0b155cd8d76110059e2562892500f00
ricardojavierah <ricardojavierah@gmail.com>
2023-05-08 19:39:48 -0500

    comentario01

---
+++ src/test/java/pe/edu/cibertec/appPracticandoClase/AppCineCibertecApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.appPracticandoClase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppCineCibertecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
